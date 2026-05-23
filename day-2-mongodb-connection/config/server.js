a8e7b8fb000dfecb89a210cbd4e6644fb5c72b70
SultanaJahan25 <coursesellerbd25@gamil.com>
2026-05-23 21:24:34 +0600

    Add MongoDB Atlas connection setup

---
+++ day-2-mongodb-connection/config/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("MongoDB Connected Project");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
