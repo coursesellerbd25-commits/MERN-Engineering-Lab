423ecb4c58b7b0bbd67e7ac7d75b9cf2e696b6ed
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-05-24 21:20:10 +0600

    Added comment

--- day-2-mongodb-connection/config/db.js
+++ day-2-mongodb-connection/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //mongoose connection
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
